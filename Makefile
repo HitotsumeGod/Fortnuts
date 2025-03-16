d043b46dff3a14ebe9e691e91bccc376b7d6f56f
peternaylor <peter@oldtiogafarm.com>
2025-03-16 09:21:02 -0400

    wine functionality fixed

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
 WCC=x86_64-w64-mingw32-gcc
 LIBS=libs/libcurl.dll.a
+DEPS=src/headers
 BUILD=build
 
-fortnutsl: src/main/windows/fortnuts.c
-	$(WCC) -o fortnuts.exe $^ -I /usr/lib/mingw64_curl/include -L /usr/lib/mingw64_curl/lib -lcurl
+fortnuts.exe: $(BUILD)/fortnuts.o
+	$(WCC) -static -o $@ $^ $(LIBS) -g
+	rm -rf $(BUILD)
+fortnuts-dynamic.exe: src/main/windows/fortnuts.c
+	$(WCC) -o $@ $^ -I $(DEPS) -L libs -lcurl
 $(BUILD)/fortnuts.o: src/main/windows/fortnuts.c
 	mkdir $(BUILD)
-	$(WCC) -c -o $@ $^ -I /usr/lib/mingw64_curl/include
-fortnuts.exe: $(BUILD)/fortnuts.o
-	$(WCC) -static -o $@ $^ $(LIBS)
-	rm -r $(BUILD)
+	$(WCC) -c -o $@ $^ -I $(DEPS)
+clean:
+	rm -f *.exe
+	rm -f *.txt
+	ls
--- src/main/windows/fortnuts.c
+++ src/main/windows/fortnuts.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <curl/curl.h>
+#include <curl.h>
 #include <windows.h>
 
 int main(void) {
 	
+	FILE * curlf;
 	CURL *c_handle;
+	CURLcode errcode;
+	char buf[CURL_ERROR_SIZE];
+	if ((curlf = fopen("curlf.txt", "w")) == NULL) {
+		perror("fopen err");
+		exit(EXIT_FAILURE);
+	}
 	if (curl_global_init(CURL_GLOBAL_WIN32) != 0) {
 		perror("global_init issue");
 		exit(EXIT_FAILURE);
@@ -17,9 +24,17 @@ int main(void) {
 	if (curl_easy_setopt(c_handle, CURLOPT_URL, "https://ipecho.net/plain") != CURLE_OK) {
 		perror("curl setop err");
        	    	exit(EXIT_FAILURE);
-	}	
-	if (curl_easy_perform(c_handle) != CURLE_OK) {
-		perror("curl perform err");
+	}
+	if (curl_easy_setopt(c_handle, CURLOPT_WRITEDATA, curlf) != CURLE_OK) {
+		perror("curl setop err");
+		exit(EXIT_FAILURE);
+	}
+	if (curl_easy_setopt(c_handle, CURLOPT_ERRORBUFFER, buf) != CURLE_OK) {
+		perror("curl setop err");
+		exit(EXIT_FAILURE);
+	}
+	if ((errcode = curl_easy_perform(c_handle)) != CURLE_OK) {
+		printf("%s\n", buf);
 		exit(EXIT_FAILURE);
 	}
 	curl_easy_cleanup(c_handle);
